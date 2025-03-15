b82ffaf7992743df6954609bb9f575a3cd06482c
Kaiwalya Patil <39062805+kaiwalya4850@users.noreply.github.com>
2025-03-15 12:38:13 +0530

    add a line on H1

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.write("Hello World")
+st.write("# This is my first project. This is H1 title")
 st.write("## This is a H2 Title!")
 x = st.text_input("Movie", "Star Wars")
 
